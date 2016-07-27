d068420f8a1ae7ea23a55c4a366f3f702e31f58f
Roderic Page <r.page@bio.gla.ac.uk>
2016-07-27 15:40:24 +0100

    Handle Index Fungorum bnode

--- process.php
+++ process.php
@@ -1,12 +1,35 @@
 <?php
 
 require_once (dirname(__FILE__) . '/vendor/arc2/ARC2.php');
+
+//----------------------------------------------------------------------------------------
+function gen_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        // 32 bits for "time_low"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_mid"
+        mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_hi_and_version",
+        // four most significant bits holds version number 4
+        mt_rand( 0, 0x0fff ) | 0x4000,
+
+        // 16 bits, 8 bits for "clk_seq_hi_res",
+        // 8 bits for "clk_seq_low",
+        // two most significant bits holds zero and one for variant DCE1.1
+        mt_rand( 0, 0x3fff ) | 0x8000,
+
+        // 48 bits for "node"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+}
 	
 
 //----------------------------------------------------------------------------------------
 function fix_rdf($xml)
 {
-	$xml = preg_replace('/\s+rdf:nodeID="bnode0"/Uu', '', $xml);
+	$xml = preg_replace('/\s+rdf:nodeID="bnode0"/Uu', ' rdf:nodeID="' . gen_uuid() . '"', $xml);
 
 	return $xml;
 }
@@ -40,7 +63,6 @@ function rdf_to_triples($xml)
 
 
 $basedir = dirname(__FILE__) . '/indexfungorum/rdf';
-$basedir = dirname(__FILE__) . '/ipni/rdf';
 
 $files = scandir($basedir);
 foreach ($files as $directory)
@@ -62,9 +84,19 @@ foreach ($files as $directory)
 				
 				if (preg_match('/^<\?xml/', $xml))
 				{
+					
+				
 					// do stuff
 					// clean
 					$xml = fix_rdf($xml);
+
+					/*					
+					$parser = ARC2::getRDFParser();		
+					$base = 'http://example.com/';
+					$parser->parse($base, $xml);	
+					$triples = $parser->getTriples();
+					print_r($triples);
+					*/
 					
 					// to N-triples
 					rdf_to_triples($xml);
