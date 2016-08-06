9b7b1ff8264a3c78c99eb6914e2005555d857050
Roderic Page <r.page@bio.gla.ac.uk>
2016-08-06 18:22:41 +0100

    Another example

--- algaebase/tosql.php
+++ algaebase/tosql.php
@@ -49,6 +49,49 @@ $rdf = '
 
 ';
 
+$rdf = '<rdf:RDF
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:dcterms="http://purl.org/dc/terms/"
+    xmlns:dwc="http://rs.tdwg.org/dwc/terms/"
+>
+    <rdf:Description rdf:about="urn:lsid:algaebase.org:taxname:60015">
+        <dc:type>ScientificName</dc:type>
+        <dc:date>2016-08-06</dc:date>
+        <dc:subject>Cryptonemia rotunda (Okamura) Kawaguchi</dc:subject>
+        <dc:title>Cryptonemia rotunda</dc:title>
+        <dc:relation>http://www.algaebase.org/search/species/detail/?species_id=Vab881bb363be741a</dc:relation>
+        <dc:creator>M.D. Guiry</dc:creator>
+        <dc:identifier>urn:lsid:algaebase.org:taxname:60015</dc:identifier>
+        <dc:publisher>AlgaeBase</dc:publisher>
+        <dc:license>http://creativecommons.org/licenses/by-nc-sa/3.0</dc:license>
+        <dc:language>Latin</dc:language>
+        <dcterms:bibliographicCitation>Kawaguchi, S. (1993). Taxonomic notes on the Halymeniaceae (Rhodophyta) from Japan, II. Japanese Journal of Phycology 41: 303-313.</dcterms:bibliographicCitation>
+        <dcterms:created>2000-11-20</dcterms:created>
+        <dcterms:modified>2014-10-24</dcterms:modified>
+
+        <dcterms:rightsHolder>AlgaeBase</dcterms:rightsHolder>
+        <dwc:empire>Eukaryota</dwc:empire>
+        <dwc:kingdom>Plantae</dwc:kingdom>
+        <dwc:phylum>Rhodophyta</dwc:phylum>
+        <dwc:subphylum>Eurhodophytina</dwc:subphylum>
+        <dwc:class>Florideophyceae</dwc:class>
+        <dwc:subclass>Rhodymeniophycidae</dwc:subclass>
+        <dwc:order>Halymeniales</dwc:order>
+        <dwc:family>Halymeniaceae</dwc:family>
+        <dwc:genus>Cryptonemia</dwc:genus>
+        <dwc:subgenus></dwc:subgenus>
+
+        <dwc:taxonRank>Species</dwc:taxonRank>
+        <dwc:ScientificName>Cryptonemia rotunda (Okamura) Kawaguchi</dwc:ScientificName>
+        <dwc:scientificNameAuthorship>(Okamura) Kawaguchi</dwc:scientificNameAuthorship>
+        <dwc:taxonomicStatus>Synonym</dwc:taxonomicStatus>
+        <dwc:scientificNameID rdf:resource="urn:lsid:algaebase.org:taxname:60015"></dwc:scientificNameID>
+
+    </rdf:Description>
+</rdf:RDF>';
+
 
 
 	$obj = new stdclass;
