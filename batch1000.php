e5b510d34c35f19788d8cc2e44007e79bd126675
Roderic Page <r.page@bio.gla.ac.uk>
2016-08-06 16:57:35 +0100

    Batch RDF files together

---
+++ batch1000.php
@@ -0,0 +1,128 @@
+<?php
+
+// Process RDF into batches of ~1000 sets of data
+
+require_once (dirname(__FILE__) . '/vendor/arc2/ARC2.php');
+
+//----------------------------------------------------------------------------------------
+function gen_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        // 32 bits for "time_low"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_mid"
+        mt_rand( 0, 0xffff ),
+
+        // 16 bits for "time_hi_and_version",
+        // four most significant bits holds version number 4
+        mt_rand( 0, 0x0fff ) | 0x4000,
+
+        // 16 bits, 8 bits for "clk_seq_hi_res",
+        // 8 bits for "clk_seq_low",
+        // two most significant bits holds zero and one for variant DCE1.1
+        mt_rand( 0, 0x3fff ) | 0x8000,
+
+        // 48 bits for "node"
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+}
+	
+
+//----------------------------------------------------------------------------------------
+function fix_rdf($xml)
+{
+	$xml = preg_replace('/\s+rdf:nodeID="bnode0"/Uu', ' rdf:nodeID="' . gen_uuid() . '"', $xml);
+
+	return $xml;
+}
+
+
+//----------------------------------------------------------------------------------------
+function rdf_to_sql($xml)
+{
+
+}
+
+//----------------------------------------------------------------------------------------
+function rdf_to_json($xml)
+{
+
+}
+
+//----------------------------------------------------------------------------------------
+function rdf_to_triples($xml)
+{
+	$parser = ARC2::getRDFParser();		
+	$base = 'http://example.com/';
+	$parser->parse($base, $xml);	
+	
+	$triples = $parser->getTriples();
+	
+	return $parser->toNTriples($triples);
+}
+
+//----------------------------------------------------------------------------------------
+
+
+$basedir = dirname(__FILE__) . '/indexfungorum/rdf';
+$basedir = '/Volumes/WD Elements 1TB/rdf-archive/indexfungorum/rdf';
+$outputdir = dirname(__FILE__) . '/indexfungorum/nt';
+
+$files = scandir($basedir);
+foreach ($files as $directory)
+{
+	if (preg_match('/^\d+$/', $directory))
+	{	
+		echo "$directory\n";
+		
+		$nt = '';
+				
+		$files = scandir($basedir . '/' . $directory);
+		
+		foreach ($files as $filename)
+		{
+			if (preg_match('/\.xml$/', $filename))
+			{	
+				$xml = file_get_contents($basedir . '/' . $directory . '/' . $filename);
+				
+				if (preg_match('/^<\?xml/', $xml))
+				{
+				
+					// do stuff
+					// clean
+					$xml = fix_rdf($xml);
+
+					/*					
+					$parser = ARC2::getRDFParser();		
+					$base = 'http://example.com/';
+					$parser->parse($base, $xml);	
+					$triples = $parser->getTriples();
+					print_r($triples);
+					*/
+					
+					// to N-triples
+					$nt .= rdf_to_triples($xml);
+					
+					
+				}
+				else
+				{
+					echo $xml;
+					exit();
+					echo $filename . "\n";
+				}
+			}
+		}
+		
+		// dump
+		file_put_contents($outputdir . '/' . $directory . '.nt', $nt);
+		
+		
+		
+		
+	}
+}
+
+
+?>
+
